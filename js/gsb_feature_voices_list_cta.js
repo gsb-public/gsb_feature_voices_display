f3a6e4ff905045d7ef87f6bb0014f4a4f3a7d071
Vijeesh Vijayan <vvijayan@CIBER-INDIA.IN>
2018-06-09 11:01:36 +0530

    WP-8349 voice display list with cta view related changes

---
+++ js/gsb_feature_voices_list_cta.js
@@ -0,0 +1,27 @@
+(function ($) {
+  Drupal.behaviors.gsb_feature_voices_display = {
+    attach: function (context, settings) {
+    	
+        $('.hide-area-one').hide();
+        $('#edit-exposed-field-academic-area-single-tid').hide();
+        $('.hide-area-two').hide();
+        $('#edit-exposed-field-field-of-study-tid').hide();
+        $("#edit-exposed-field-person-type-tid").on('change', function() {
+           		if($("#edit-exposed-field-person-type-tid").val() == '42776'){
+        			$('.hide-area-one').show();
+        			$('#edit-exposed-field-academic-area-single-tid').show();
+        			$('.hide-area-two').hide();
+        			$('#edit-exposed-field-field-of-study-tid').hide();
+        		}
+        		if($("#edit-exposed-field-person-type-tid").val() == '42751'){
+        			$('.hide-area-two').show();
+        			$('#edit-exposed-field-field-of-study-tid').show();
+        			$('.hide-area-one').hide();
+        			$('#edit-exposed-field-academic-area-single-tid').hide();
+        		}
+        });
+
+    }
+  };
+})(jQuery);
+
